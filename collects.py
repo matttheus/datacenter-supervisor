3c94f1192d0f67c9fb120c04fee30cf318770dc1
Matheus <matheusphranco@gmail.com>
2019-09-27 21:59:27 -0300

    Content collection code added

--- collects.py
+++ collects.py
@@ -1,7 +1,7 @@
 import psycopg2
 import serial
 import io
-from time import time
+from time import sleep
 from datetime import datetime
 from random import randint
 
@@ -13,10 +13,10 @@ def db_connect():
 	return con, cursor
 
 
-def insert_from_serial(table, sensor, value, date):
+def insert_from_serial(table, temperature, humidity, gas, date):
 	con, cursor = db_connect()
 	table = table
-	cursor.execute(f"INSERT INTO {table} ({sensor}, date) VALUES ({value}, '{date}');")
+	cursor.execute(f"INSERT INTO {table} (temperature, humidity, gas, date) VALUES ({temperature}, {humidity}, {gas}, '{date}');")
 	con.commit()
 	con.close()
 	print('Insert accomplished!')
@@ -29,14 +29,8 @@ def insert_from_serial(table, sensor, value, date):
 print('Server started...')
 while 1:
 	date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	insert_from_serial('main_temperature', 'temperature', randint(1, 10), date)
-	
-	date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	insert_from_serial('main_gas', 'gas', randint(1, 10), date)
-	
-	date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	insert_from_serial('main_humidity', 'humidity', randint(1, 10), date)
-
+	insert_from_serial('main_collection', randint(1, 10), randint(1, 10), randint(1, 10), date)
+	sleep(1)
 ''' 
 # Serial code
 se = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
