e28b7a6f729e6004adc43fb7ed35e1ac55c237a4
Matheus <matheusphranco@gmail.com>
2019-11-28 16:47:02 -0300

    Adiciona pequenas modificações

--- collects.py
+++ collects.py
@@ -1,25 +1,25 @@
-import psycopg2
 import serial
-import io
-from time import sleep
-from datetime import datetime
-from random import randint
-
-# Make connection with the database
-def db_connect():
-	con = psycopg2.connect("dbname=datacentersupervisor user=postgres host=localhost password=postgres") 
-	cursor = con.cursor()
-
-	return con, cursor
-
-
-def insert_from_serial(table, temperature, humidity, gas, date):
-	con, cursor = db_connect()
-	table = table
-	cursor.execute(f"INSERT INTO {table} (temperature, humidity, gas, date) VALUES ({temperature}, {humidity}, {gas}, '{date}');")
-	con.commit()
-	con.close()
-	print('Insert accomplished!')
+# import psycopg2
+# import io
+# from time import sleep
+# from datetime import datetime
+# from random import randint
+#
+# # Make connection with the database
+# def db_connect():
+# 	con = psycopg2.connect("dbname=datacentersupervisor user=postgres host=localhost password=postgres")
+# 	cursor = con.cursor()
+#
+# 	return con, cursor
+#
+#
+# def insert_from_serial(table, temperature, humidity, gas, date):
+# 	con, cursor = db_connect()
+# 	table = table
+# 	cursor.execute(f"INSERT INTO {table} (temperature, humidity, gas, date) VALUES ({temperature}, {humidity}, {gas}, '{date}');")
+# 	con.commit()
+# 	con.close()
+# 	print('Insert accomplished!')
 
 
 #---------------------------------------------------------------------
@@ -27,21 +27,22 @@ def insert_from_serial(table, temperature, humidity, gas, date):
 #---------------------------------------------------------------------
 
 print('Server started...')
-while 1:
-	date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	insert_from_serial('main_collection', randint(1, 10), randint(1, 10), randint(1, 10), date)
-	sleep(1)
-''' 
+# while 1:
+# 	date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# 	insert_from_serial('main_collection', randint(1, 10), randint(1, 10), randint(1, 10), date)
+# 	sleep(1)
+
 # Serial code
 se = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+
 list = []
-a = str(time())
-tmp_now = a.split('.')[0]
+# a = str(time())
+# tmp_now = a.split('.')[0]
 
 while True:
 	if not se.readline().decode('utf-8') == '':
 		print(se.readline().decode('utf-8'))
-'''
+
 
 # tmp_serial_data = 'FALTANDO CÓDIGO DO SERIAL'
 
@@ -52,4 +53,3 @@ while True:
 
 # 	if tmp_serial_data != _serial_data:
 # 		tmp_serial_data = _serial_data
-
--- datacentersupervisor/main/urls.py
+++ datacentersupervisor/main/urls.py
@@ -3,6 +3,6 @@ from .views import CollectionView, IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('index', IndexView.as_view(), name='index'),
+    path('index/', IndexView.as_view(), name='index'),
     path('collect/', CollectionView.as_view(), name='collect'),
 ]
