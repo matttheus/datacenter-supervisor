684383f1d176bbe0162b18c6e59b7c6d190290f1
Matheus <matheusphranco@gmail.com>
2019-12-01 19:46:12 -0300

    Add thread insert funcionality

--- collects.py
+++ collects.py
@@ -10,7 +10,8 @@ def db_connect():
 	cursor = con.cursor()
 	return con, cursor
 
-def insert_from_serial(temperature, humidity, gas, date):
+def insert_from_serial(temperature, humidity, gas, date, time_sleep):
+	sleep(time_sleep)
 	con, cursor = db_connect()
 	cursor.execute(f"INSERT INTO main_collection (temperature, humidity, gas, date) VALUES ({temperature}, {humidity}, {gas}, '{date}');")
 	con.commit()
@@ -25,6 +26,8 @@ print('Server started...')
 
 # Serial code
 se = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+mq2_sleep_time = 10
+dht11_sleep_time = 20
 
 while True:
 	# Reading serial
@@ -33,6 +36,12 @@ while True:
 		if data:
 			data_clear = data.decode().split("|")
 			date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-			insert_from_serial(data_clear[0], data_clear[1], data_clear[2], date)
+			temperature = 0 # data_clear
+			humidity = 0 # data_clear
+			mq2 = 0 # data_clear
+			dht11_th = Thread(target=insert_from_serial, args=(temperature, humidity, None, dht11_sleep_time, ), name="DHT11-Thread")
+			dht11_th.start()
+			mq2_th = Thread(target=insert_from_serial, args=(None, None, mq2, mq2_sleep_time, ), name="MQ2-Thread")
+			mq2_th.start()
 	except Exception as err:
 		print(f"Ocorreu um problema: {err}")
