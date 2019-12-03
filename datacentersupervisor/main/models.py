8e347f748f53500d064721430028b18f946cbc46
Matheus <matheusphranco@gmail.com>
2019-12-03 15:44:21 -0300

    Set fields to null-able

--- datacentersupervisor/main/models.py
+++ datacentersupervisor/main/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 # Create your models here.
 
 class Collection(models.Model):
-    temperature = models.FloatField()
-    humidity = models.FloatField()
-    gas = models.FloatField()
+    temperature = models.FloatField(null=True, blank=True)
+    humidity = models.FloatField(null=True, blank=True)
+    gas = models.FloatField(null=True, blank=False)
     date = models.DateTimeField(default=datetime.now, blank=True)
